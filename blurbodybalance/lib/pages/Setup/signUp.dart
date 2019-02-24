700a0c19aa3a7b3a9b9e13cbc561fe991b2be33e
Anthony Arbour <anthony.arbour@maine.edu>
2019-02-23 20:03:06 -0500

    Updated signUp code

--- blurbodybalance/lib/pages/Setup/signUp.dart
+++ blurbodybalance/lib/pages/Setup/signUp.dart
@@ -1,3 +1,5 @@
+import 'package:blurbodybalance/pages/Setup/signIn.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -5,8 +7,57 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              validator: (input) {
+                if (input.isEmpty) {
+                  return 'Please type an email address!';
+                }
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (input) {
+                if (input.length < 6) {
+                  return 'Your password needs to be at least 6 characters';
+                }
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            RaisedButton(
+              onPressed: signUp,
+              child: Text('Sign Up'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> signUp() async {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      try {
+        await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: _email, password: _password);
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
+      } catch (e) {
+        print(e.message);
+      }
+    }
   }
 }
