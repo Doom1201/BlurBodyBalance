e1e1927d9379891d22dea4cb5633f6285fa1abd5
Jacob R Boudreau <jacob.boudreau8@gmail.com>
2019-04-04 18:07:14 -0400

    Overflow fix
    Fixes screen overflow on nexus 5. Not sure if this is the best way to do it for all phones. I just shifted items on the screen up.

--- blurbodybalance/lib/pages/Setup/welcome.dart
+++ blurbodybalance/lib/pages/Setup/welcome.dart
@@ -27,19 +27,19 @@ class _WelcomePageState extends State<WelcomePage> {
                   child: Stack(
                     children: <Widget>[
                       Container(
-                        padding: EdgeInsets.fromLTRB(15.0, 110.0, 0.0, 0.0),
+                        padding: EdgeInsets.fromLTRB(15.0, 50.0, 0.0, 0.0),
                         child: Text('Hello',
                             style: TextStyle(
                                 fontSize: 80.0, fontWeight: FontWeight.bold)),
                       ),
                       Container(
-                        padding: EdgeInsets.fromLTRB(16.0, 175.0, 0.0, 0.0),
+                        padding: EdgeInsets.fromLTRB(16.0, 115.0, 0.0, 0.0),
                         child: Text('There',
                             style: TextStyle(
                                 fontSize: 80.0, fontWeight: FontWeight.bold)),
                       ),
                       Container(
-                        padding: EdgeInsets.fromLTRB(220.0, 175.0, 0.0, 0.0),
+                        padding: EdgeInsets.fromLTRB(220.0, 115.0, 0.0, 0.0),
                         child: Text('.',
                             style: TextStyle(
                                 fontSize: 80.0,
