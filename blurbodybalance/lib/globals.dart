b7c04263f4fbad14e27ab5e1e546812d948b28c3
spillbeans <47337116+spillbeans@users.noreply.github.com>
2019-04-06 23:32:42 -0400

    Added "Mean" Message functionality
    Added Mean message global boolean, as well as lists of messages for specific situations and a randomizer function.

--- blurbodybalance/lib/globals.dart
+++ blurbodybalance/lib/globals.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-bool isDark;
+bool isDark = false;
+bool meanMessageOn = true;
+
 
 // This function controls the dark mode functionality
 Color dark() {
@@ -11,4 +14,42 @@ Color dark() {
     curCol = Color.fromRGBO(29, 29, 29, 90);
   }
   return curCol;
+}
+
+//Insult bank (REMEMBER, GO FOR FUNNY NOT MEAN, ESPECIALLY FOR WEIGHT) 
+List<String> generic = new List<String>();// List of generic insults for goals not met.
+List<String> overCal = new List<String>();  //insults for going over calorie limit
+List<String> underRunGoal = new List<String>(); //insults for not making run goal for the week
+
+generic.add('Oh, there you are. Try to do something productive while youre here.');
+generic.add('Oh hey. Need a hand to put yourself back on the bandwagon?');
+generic.add('Huh, is it New Years already? Because I haven\'t seen any resolutions for a while.');
+
+overCal.add('Welcome back. Put down the sandwich, we have work to do.');
+overCal.add('Welcome back. Put down the cup of whipped cream you call coffee, we have work to do.');
+
+underRunGoal.add('Are you jogging? No? Then get to it!');
+underRunGoal.add('I don\'t see legs moving!');
+
+
+//This function returns a random insult of the specified category.
+//IMPORTANT: MAKE SURE MEANMESSAGES IS FALSE BEFORE INVOKING
+String getInsult(String cat) {
+  var rng;                               //random number generator
+  switch (cat) {
+    case 'gen':
+      rng = new Random();
+      int i = rng.nextInt();
+      return generic[i];
+    case 'overC':
+      rng = new Random();
+      int i = rng.nextInt();
+      return overCal[i];
+    case 'underRunG':
+      rng = new Random();
+      int i = rng.nextInt();
+      return underRunGoal[i];
+    default:
+      throw Exception('Improper category given');
+  }
 }
