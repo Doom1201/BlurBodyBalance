f4acd316a2b71a916b02083caf77d6262834de2f
spillbeans <47337116+spillbeans@users.noreply.github.com>
2019-04-09 16:32:48 -0400

    Positive Messages + modularity
    Added several positive messages to the comment bank. Also methodized some repeating code and added thrown exceptions to prevent mean messages from occurring if the option has not been turned on.

--- blurbodybalance/lib/globals.dart
+++ blurbodybalance/lib/globals.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 bool isDark;
+bool meanMsg; // T = ON, F = OFF
 
 
 // This function controls the dark mode functionality
@@ -15,16 +16,17 @@ Color dark() {
   return curCol;
 }
 
-//Insult bank (REMEMBER, GO FOR FUNNY NOT MEAN, ESPECIALLY FOR WEIGHT) 
-List<String> badGeneric = new List<String>();// List of generic insults for goals not met.
-List<String> badOverCal = new List<String>();  //insults for going over calorie limit
-List<String> badUnderRunGoal = new List<String>(); //insults for not making run goal for the week
-
-
-
-//This function returns a random insult of the specified category
+//This function returns a random comment of the specified category
 String getMessage(String cat) {
-  var rng;                               //random number generator
+
+//Comment bank (REMEMBER, GO FOR FUNNY NOT MEAN, ESPECIALLY FOR WEIGHT)
+  List<String> badGeneric = new List<String>();       // List of generic insults for goals not met.
+  List<String> badOverCal = new List<String>();       // insults for going over calorie limit
+  List<String> badUnderRunGoal = new List<String>();  //insults for not making run goal for the week
+  List<String> goodGeneric = new List<String>();      // List of positive generic comments
+  List<String> goodUnderCal = new List<String>();     //List of positive calorie comments
+  List<String> goodOverRunGoal = new List<String>();  //List of positive run comments
+
   badGeneric.add('Oh, there you are. Try to do something productive while youre here.');
   badGeneric.add('Oh hey. Need a hand to put yourself back on the bandwagon?');
   badGeneric.add('Huh, is it New Years already? Because I haven\'t seen any resolutions for a while.');
@@ -33,21 +35,43 @@ String getMessage(String cat) {
   badOverCal.add('Welcome back. Put down the cup of whipped cream you call coffee, we have work to do.');
 
   badUnderRunGoal.add('Are you jogging? No? Then get to it!');
-  badUnderRunGoal.add('I don\'t see legs moving!');
+  badUnderRunGoal.add('C\'mon, buddy! I don\'t see legs moving!');
+
+  goodGeneric.add('Hey there! Let\'s get started, shall we?');
+  goodGeneric.add('Welcome back! Ready to begin?');
+  goodGeneric.add('Good to see you again! Now, let\'s get down to business!');
+  goodGeneric.add('Hello again! You\'re doing good so far, let\'s keep it going!');
+
+  goodUnderCal.add('You\'ve got this. Grab a (healthy) bagel and let\'s do this!');
+  goodUnderCal.add('You\'re doing great so far. Remember: The less cals you have now, the bigger your dessert!');
+
+  goodOverRunGoal.add('Holy footprints that\'s a lotta steps! Keep it up!');
+  goodOverRunGoal.add('Phew, slow down! I need to keep up!');
+
+  //Depending on which case is passed, the corresponding category is looked at
   switch (cat) {
     case 'badGen':
-      rng = new Random();
-      int i = rng.nextInt();
-      return badGeneric[i];
+      if (!meanMsg) throw Exception('Mean messages are turned off.');
+      return msgHelp(badGeneric);
     case 'badOverC':
-      rng = new Random();
-      int i = rng.nextInt();
-      return badOverCal[i];
+      if (!meanMsg) throw Exception('Mean messages are turned off.');
+      return msgHelp(badOverCal);
     case 'badUnderRunG':
-      rng = new Random();
-      int i = rng.nextInt();
-      return badUnderRunGoal[i];
+      if (!meanMsg) throw Exception('Mean messages are turned off.');
+      return msgHelp(badUnderRunGoal);
+    case 'goodGen':
+      return msgHelp(goodGeneric);
+    case 'goodUnderC':
+      return msgHelp(goodUnderCal);
+    case 'goodOverRungG':
+      return msgHelp(goodOverRunGoal);
     default:
       throw Exception('Improper category given');
   }
+}
+
+String msgHelp(List<String> a) {
+  var rng = new Random();         // Random number generator
+  int i = rng.nextInt(a.length);  // Pick a random message
+  return a[i];
 }
