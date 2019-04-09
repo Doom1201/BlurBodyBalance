ee572567d4d00e69d11133864bba873a8228d927
Anthony Arbour <anthony.arbour@maine.edu>
2019-04-09 15:40:09 -0400

    Update globals.dart

--- blurbodybalance/lib/globals.dart
+++ blurbodybalance/lib/globals.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-bool isDark = true;
+// This boolean determines if we want dark mode on or off at startup
+bool isDark = false;
+
+// This class handles anything involving colors including darkmode
+class ColorHandler {
+  // This function handles the dark mode color for
+  // Backgrounds
+  Color bgDark() {
+    Color bgCol;
+    if (!isDark) {
+      bgCol = Colors.white;
+    } else {
+      bgCol = Colors.black;
+    }
+    return bgCol;
+  }
+
+  // This function handles the dark mode color for
+  // Backgrounds
+  Color barDark() {
+    Color barCol;
+    if (!isDark) {
+      barCol = Colors.white;
+    } else {
+      barCol = Color.fromRGBO(29, 29, 29, 90);
+    }
+    return barCol;
+  }
+
+  // This function handles the dark mode color for
+  // Icons and Text
+  Color iconDark() {
+    Color iconCol;
+    if (isDark) {
+      iconCol = Colors.white;
+    } else {
+      iconCol = Colors.black;
+    }
+
+    return iconCol;
+  }
+}
 
 
 // This function controls the dark mode functionality
