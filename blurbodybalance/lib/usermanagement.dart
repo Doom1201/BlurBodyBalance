4bd04243f4a2ee5810cfb1e386e5f21d1740b48d
sbeaudoinumaine <samuel.beaudoin@maine.edu>
2019-04-16 00:04:48 -0400

    UserDataManagement: AddWeightEntry, getData (broken rn)

--- blurbodybalance/lib/usermanagement.dart
+++ blurbodybalance/lib/usermanagement.dart
@@ -1,12 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class UserManagement {
-  storeNewUser(user, context) {
-    Firestore.instance.collection('/users').add({
+class UserDataManagement {
+  storeNewUser(user) {
+    Firestore.instance.collection('users').document(user.email).setData({
       'email': user.email,
-      'uid': user.uid
+      'uid': user.uid,
+      'weightData': []
     }).catchError((e) {
       print(e);
     });
+  }
+
+  addWeightEntry(user, weight) {
+    Firestore.instance.collection('users').document(user.email).updateData({'weightData':FieldValue.arrayUnion(
+      [{
+      'weight': weight,
+      'timestamp': Timestamp.now()}]
+      )});
+  }
+
+  getData(user) {
+    DocumentSnapshot snapshot;
+    Firestore.instance.collection('users').document(user.email).get().then((doc) {
+      snapshot = doc;
+      print("weightData");
+    }).catchError((e) {
+      print("ERROR");
+      print(e);
+    });
+    print(snapshot['weightData']);
   }
 }
