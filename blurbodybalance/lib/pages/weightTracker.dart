6d5d6ccbfaea229c54147f64fe6cdac6b29656d8
sbeaudoinumaine <samuel.beaudoin@maine.edu>
2019-04-16 23:02:07 -0400

    Rough version of Last Weight Entered + Dialog

--- blurbodybalance/lib/pages/weightTracker.dart
+++ blurbodybalance/lib/pages/weightTracker.dart
@@ -17,28 +17,77 @@ class _WeightTrackerState extends State<WeightTracker> {
     //collect weight data
     getWeightData();
 
-    final lastWeightEntry = Text("Weight Tracker");
-    
+    final lastWeightEntry = Center(
+        child: Column(
+      children: <Widget>[
+        Text("Latest Weight Entry"),
+        Text("###.#"),
+        RaisedButton(
+          child: Text("Enter Weight"),
+          elevation: 4.0,
+          color: Colors.blue,
+          textColor: Colors.white,
+          onPressed: () {
+            inputDialog(context);
+          },
+        ),
+      ],
+    ));
+
     //final goalProgress
     //final weightChart
 
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            lastWeightEntry,
-            //GoalProgress,
-            //WeightChart,
-          ],
-        )
-      ),
-    );
+        body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        lastWeightEntry,
+      ],
+    ));
+  }
+
+  Future<bool> inputDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return new SimpleDialog(
+            title: new Text("Enter Weight"),
+            children: <Widget>[
+              Center(
+                child: new TextField(
+                  decoration: new InputDecoration(labelText: "Weight"),
+                  keyboardType: TextInputType.numberWithOptions(
+                      signed: false, decimal: true),
+                  onChanged: (string) {
+                    if (string.contains(' ') ||
+                        string.contains(',') ||
+                        string.contains('-')) {
+                      string = string.substring(0, string.length - 1);
+                    }
+                  },
+                  onSubmitted: (string) async {
+                    //get user
+                    FirebaseUser user =
+                        await FirebaseAuth.instance.currentUser();
+                    //get entered weight
+                    double weight = double.parse(string);
+                    //store weight in database
+                    UserDataManagement().addWeightEntry(user, weight);
+                    //close dialog
+                    Navigator.of(context).pop();
+                  },
+                ),
+              )
+            ],
+          );
+        });
   }
 
   getWeightData() async {
     UserDataManagement dataManager = new UserDataManagement();
     //get current firebase user
-    FirebaseUser user = await FirebaseAuth.instance.currentUser().catchError((error) {
+    FirebaseUser user =
+        await FirebaseAuth.instance.currentUser().catchError((error) {
       print(error);
     });
     _weightData = await dataManager.getWeightData(user);
